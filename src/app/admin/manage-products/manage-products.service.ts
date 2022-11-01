01fc9cc47e71ee05f188eb315e870245eb5c641f
Alina Kohut <alina_kohut@epam.com>
2022-11-01 19:43:15 +0200

    feat: change url for import service

--- src/app/admin/manage-products/manage-products.service.ts
+++ src/app/admin/manage-products/manage-products.service.ts
@@ -33,9 +33,7 @@ export class ManageProductsService extends ApiService {
     const url = this.getUrl('import', 'import');
 
     return this.http.get<string>(url, {
-      params: {
-        name: fileName,
-      },
+      params: { fileName },
     });
   }
 }
